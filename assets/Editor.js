103f9da24115fa85975ada35b39a30c0dfac1f4a
Ryan McCue <me@ryanmccue.info>
2017-06-06 15:24:42 +1000

    Add IDs for Quick Edit form elements

--- assets/Editor.js
+++ assets/Editor.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 export default class Editor extends React.Component {
 	render() {
-		const { value, onChange } = this.props;
+		const { id, value, onChange } = this.props;
 
 		return <div id="wp-replycontent-wrap" className="wp-core-ui wp-editor-wrap html-active">
 			<div id="wp-replycontent-editor-container" className="wp-editor-container">
 				<textarea
 					className="wp-editor-area"
-					id="replycontent"
+					id={ id }
 					rows={ 20 }
 					cols={ 40 }
 					value={ value }
@@ -18,3 +18,6 @@ export default class Editor extends React.Component {
 		</div>;
 	}
 }
+Editor.defaultProps = {
+	id: "replycontent",
+};
