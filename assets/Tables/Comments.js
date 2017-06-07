40fee526587eea0e793c6850eceb2320478b3397
Ryan McCue <me@ryanmccue.info>
2017-06-07 15:56:49 +1000

    Move comment-specific columns to the table

--- assets/Tables/Comments.js
+++ assets/Tables/Comments.js
@@ -1,8 +1,41 @@
 import React from 'react';
 
-import columns from '../columns';
 import ListTable from '../ListTable';
 import CollectionTable from '../CollectionTable';
+import ColumnCheckbox from '../Columns/Checkbox';
+import ColumnAuthor from '../Columns/Author';
+import ColumnContent from '../Columns/Content';
+import ColumnResponse from '../Columns/Response';
+import ColumnDate from '../Columns/Date';
+
+const columns = {
+	cb: {
+		label: '',
+		className: 'column-cb check-column',
+		component: ColumnCheckbox,
+	},
+	author: {
+		label: 'Author',
+		className: 'author column-author',
+		component: ColumnAuthor,
+	},
+	comment: {
+		label: 'Comment',
+		className: 'comment column-comment',
+		component: ColumnContent,
+	},
+	response: {
+		label: 'In Response To',
+		className: 'response column-response',
+		component: ColumnResponse,
+	},
+	date: {
+		label: 'Submitted On',
+		className: 'date column-date',
+		component: ColumnDate,
+	},
+};
+
 
 export default class Comments extends React.Component {
 	constructor( props ) {
